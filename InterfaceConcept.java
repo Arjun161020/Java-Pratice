f993d1d54c1c454022b045f6b7ad230024510ecd
Arjun C <145429142+Arjun161020@users.noreply.github.com>
2026-03-03 00:18:03 +0530

    Update InterfaceConcept.java

--- InterfaceConcept.java
+++ InterfaceConcept.java
@@ -35,3 +35,42 @@ public class Main
 		c.sound();
 	}
 }
+
+
+
+// //interface
+// interface Checking{
+//      void add(int a,int b);
+//      void sound();
+     
+//      default void method2() {
+//         System.out.println("This is a default method");
+//     }
+     
+// }
+
+// class car implements Checking{
+    
+//     @Override
+//     public void add(int a , int b){
+//         System.out.println("Car Brands BMW BENZ JAGUAR AUDI "+ (a+b));
+//     }
+    
+//     @Override
+//     public void sound(){
+//         System.out.println("These automobile sound is very nice to hear");
+//     }
+// }
+
+
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 		System.out.println("****Abstraction*****");
+
+// 		Checking c = new car();
+// 		c.method2();
+// 		c.add(10,20);
+// 		c.sound();
+// 	}
+// }
